fe53ebbbd9f76af11f3317b2c1d78a7fbb9b080a
Foysal <foysaljoarder012@gmail.com>
2026-01-02 11:53:18 +0600

    Settings Page UI Design

--- lib/modules/search/view/search_screen.dart
+++ lib/modules/search/view/search_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:material_symbols_icons/symbols.dart';
+import '../../../core/constants/app_image.dart';
+import '../../home/view/home_screen.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,10 +12,329 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+
+  final searchTextController = TextEditingController();
+
+  List<Product> productList = [
+    Product(
+        productImage: AppImage.productImage1,
+        productName: 'Sara watch model xx78',
+        ProductNewPrice: '950',
+        productOldPrice: '1860',
+        productDiscount: '65',
+        productRating: '4.5',
+        productRatingNumber: '850'
+    ),
+    Product(
+        productImage: AppImage.productImage2,
+        productName: 'Gibson watch model xx88',
+        ProductNewPrice: '850',
+        productOldPrice: '1960',
+        productDiscount: '53',
+        productRating: '4.3',
+        productRatingNumber: '320'
+    ),
+    Product(
+        productImage: AppImage.productImage3,
+        productName: 'Apex watch model xx48',
+        ProductNewPrice: '750',
+        productOldPrice: '1560',
+        productDiscount: '46',
+        productRating: '4.2',
+        productRatingNumber: '550'
+    ),
+    Product(
+        productImage: AppImage.productImage4,
+        productName: 'Bata watch model xx78',
+        ProductNewPrice: '450',
+        productOldPrice: '1260',
+        productDiscount: '82',
+        productRating: '4.1',
+        productRatingNumber: '910'
+    ),
+    Product(
+        productImage: AppImage.productImage5,
+        productName: 'Sara shoe model xx68',
+        ProductNewPrice: '350',
+        productOldPrice: '1960',
+        productDiscount: '35',
+        productRating: '3.5',
+        productRatingNumber: '550'
+    ),
+    Product(
+        productImage: AppImage.productImage6,
+        productName: 'Zara shoe model xx98',
+        ProductNewPrice: '450',
+        productOldPrice: '1960',
+        productDiscount: '35',
+        productRating: '3.5',
+        productRatingNumber: '550'
+    ),
+    Product(
+        productImage: AppImage.productImage7,
+        productName: 'Sara dress model xx78',
+        ProductNewPrice: '950',
+        productOldPrice: '1860',
+        productDiscount: '65',
+        productRating: '4.5',
+        productRatingNumber: '850'
+    ),
+    Product(
+        productImage: AppImage.productImage8,
+        productName: 'Zara dress model xx78',
+        ProductNewPrice: '950',
+        productOldPrice: '1860',
+        productDiscount: '65',
+        productRating: '4.5',
+        productRatingNumber: '850'
+    ),
+    Product(
+        productImage: AppImage.productImage9,
+        productName: 'Hujaifa Mens model xx78',
+        ProductNewPrice: '950',
+        productOldPrice: '1860',
+        productDiscount: '65',
+        productRating: '4.5',
+        productRatingNumber: '850'
+    ),
+    Product(
+        productImage: AppImage.productImage10,
+        productName: 'Hannan Mens model xx78',
+        ProductNewPrice: '950',
+        productOldPrice: '1860',
+        productDiscount: '65',
+        productRating: '4.5',
+        productRatingNumber: '850'
+    ),
+    Product(
+        productImage: AppImage.productImage11,
+        productName: 'Kids dress model xx78',
+        ProductNewPrice: '950',
+        productOldPrice: '1860',
+        productDiscount: '65',
+        productRating: '4.5',
+        productRatingNumber: '850'
+    ),
+    Product(
+        productImage: AppImage.productImage12,
+        productName: 'Kids dress model xx78',
+        ProductNewPrice: '950',
+        productOldPrice: '1860',
+        productDiscount: '65',
+        productRating: '4.5',
+        productRatingNumber: '850'
+    ),
+    Product(
+        productImage: AppImage.productImage13,
+        productName: 'Mobile model xx78',
+        ProductNewPrice: '950',
+        productOldPrice: '1860',
+        productDiscount: '65',
+        productRating: '4.5',
+        productRatingNumber: '850'
+    ),
+    Product(
+        productImage: AppImage.productImage14,
+        productName: 'Mobile model xx78',
+        ProductNewPrice: '950',
+        productOldPrice: '1860',
+        productDiscount: '65',
+        productRating: '4.5',
+        productRatingNumber: '850'
+    )
+  ];
+
+  @override
+  void dispose() {
+    searchTextController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height;
+    final width = MediaQuery.sizeOf(context).width;
     return Scaffold(
-      backgroundColor: Colors.amber,
+      backgroundColor: Color(0xffF2F2F2),
+      appBar: AppBar(
+        backgroundColor: Color(0xffF2F2F2),
+        scrolledUnderElevation: 0.0,
+        leading: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: CircleAvatar(
+            maxRadius: 10,
+            backgroundColor: Colors.black12,
+            child: Icon(Symbols.menu_open),
+          ),
+        ),
+
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('${AppImage.splashLogo}', height: 50, width: 35),
+            Gap(5.0),
+            Text('Stylish',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff4392F9)
+              ),
+            )
+          ],
+        ),
+        // centerTitle: true,
+
+        actions: [
+          CircleAvatar(
+              radius: 25,
+              backgroundImage:AssetImage('${AppImage.profileImage}')
+          ),
+          Gap(10.0)
+        ],
+      ),
+
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+
+        child: CustomScrollView(
+          scrollDirection: Axis.vertical,
+          slivers: [
+
+            // Search Text Field
+            SliverToBoxAdapter(
+              child: TextFormField(
+                controller: searchTextController,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Symbols.search, color: Color(0xffA8A8A9)),
+                    hintText: 'Search any product...',
+                    hintStyle: TextStyle(
+                        fontSize: 12,
+                        color: Color(0xff676767),
+                        fontWeight: FontWeight.w500
+                    ),
+                    suffixIcon: IconButton(
+                        onPressed: (){
+
+                        },
+                        icon: Icon(Symbols.mic)
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    // enabledBorder: OutlineInputBorder(
+                    //     borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    //     borderSide: BorderSide(
+                    //         width: 2,
+                    //         color: Color(0xffA8A8A9)
+                    //     )
+                    // ),
+                    // focusedBorder: OutlineInputBorder(
+                    //     borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    //     borderSide: BorderSide(
+                    //         width: 2,
+                    //         color: Color(0xffA8A8A9)
+                    //     )
+                    // )
+                    border: InputBorder.none
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(child: Gap(16.0)),
+
+            // All featured || sort || filter
+            SliverToBoxAdapter(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('All Featured',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Color(0xff000000),
+                        fontWeight: FontWeight.w500
+                    ),
+                  ),
+
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(5.0))
+                        ),
+                        child: Row(
+                          children: [
+                            Text('Sort',
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500
+                              ),
+                            ),
+                            Gap(5.0),
+                            Icon(Symbols.swap_vert, size: 15)
+                          ],
+                        ),
+                      ),
+                      Gap(10.0),
+
+                      Container(
+                        padding: EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(5.0))
+                        ),
+                        child: Row(
+                          children: [
+                            Text('Filter',
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500
+                              ),
+                            ),
+                            Gap(5.0),
+                            Icon(Symbols.filter_alt, size: 15)
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SliverToBoxAdapter(child: Gap(16.0)),
+
+            // Offer Product
+            SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                  final productInfo = productList[index];
+                  return Align(
+                    alignment: AlignmentGeometry.center,
+                    child: ProductMiniCard(
+                      productImage: productInfo.productImage ?? '',
+                      productName: productInfo.productName ?? '',
+                      productNewPrice: '\$${productInfo.ProductNewPrice}',
+                      productOldPrice: '\$${productInfo.productOldPrice}',
+                      productDiscount: '${productInfo.productDiscount}',
+                      productRating: double.tryParse('${productInfo.productRating}'),
+                      productRatingNumber: '${productInfo.productRatingNumber}',
+                    ),
+                  );
+                },
+                childCount: productList.length,
+              ),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 0.95,
+              ),
+            ),
+            SliverToBoxAdapter(child: Gap(50.0)),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/modules/settings/view/settings_screen.dart
+++ lib/modules/settings/view/settings_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -10,8 +12,218 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height;
+    final width = MediaQuery.sizeOf(context).width;
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Color(0xffF2F2F2),
+      appBar: AppBar(
+        backgroundColor: Color(0xffF2F2F2),
+        leadingWidth: 40,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10.0),
+          child: Container(
+            height: 20,
+            width: 20,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.grey[500],
+            ),
+            child: Icon(Symbols.arrow_back),
+          ),
+        ),
+
+        title: Text('Account', style: TextStyle(fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold)),
+        centerTitle: true
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                color: Colors.grey[300]
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                          color: Colors.purple
+                        ),
+                      ),
+                      Gap(10.0),
+
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Foysal Joarder',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black
+                            ),
+                          ),
+                          Text('foysal888@gmail.com',
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black38
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+
+                  Icon(Symbols.edit)
+                ],
+              ),
+            ),
+            Gap(10.0),
+
+            Text('Settings', style: TextStyle(fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold)),
+            Gap(5.0),
+            
+            ListTile(
+              tileColor: Colors.grey[300],
+              leading: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: Colors.white
+                ),
+                child: Icon(Symbols.lab_profile),
+              ),
+              
+              title: Text('Account Details', style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold)),
+              
+              trailing: Icon(Icons.arrow_forward_ios_outlined),
+            ),
+            Gap(10.0),
+
+            ListTile(
+              tileColor: Colors.grey[300],
+              leading: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.white
+                ),
+                child: Icon(Symbols.notification_add),
+              ),
+
+              title: Text('Notifications', style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold)),
+
+              trailing: Icon(Icons.arrow_forward_ios_outlined),
+            ),
+            Gap(10.0),
+
+            ListTile(
+              tileColor: Colors.grey[300],
+              leading: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.white
+                ),
+                child: Icon(Symbols.email),
+              ),
+
+              title: Text('Email', style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold)),
+
+              trailing: Icon(Icons.arrow_forward_ios_outlined),
+            ),
+            Gap(10.0),
+
+            ListTile(
+              tileColor: Colors.grey[300],
+              leading: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.white
+                ),
+                child: Icon(Symbols.moved_location),
+              ),
+
+              title: Text('Location Services', style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold)),
+
+              trailing: Icon(Icons.arrow_forward_ios_outlined),
+            ),
+            Gap(10.0),
+
+            ListTile(
+              tileColor: Colors.grey[300],
+              leading: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.white
+                ),
+                child: Icon(Symbols.logout),
+              ),
+
+              title: Text('Log Out', style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold)),
+
+              trailing: Icon(Icons.arrow_forward_ios_outlined),
+            ),
+            Gap(10.0),
+
+            Text('Support', style: TextStyle(fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold)),
+            Gap(5.0),
+
+            ListTile(
+              tileColor: Colors.grey[300],
+              leading: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.white
+                ),
+                child: Icon(Symbols.help),
+              ),
+
+              title: Text('Help Center', style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold)),
+
+              trailing: Icon(Icons.arrow_forward_ios_outlined),
+            ),
+            Gap(10.0),
+
+            ListTile(
+              tileColor: Colors.grey[300],
+              leading: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.white
+                ),
+                child: Icon(Symbols.terminal_sharp),
+              ),
+
+              title: Text('Terms & Conditions', style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold)),
+
+              trailing: Icon(Icons.arrow_forward_ios_outlined),
+            ),
+            Gap(10.0),
+          ],
+        ),
+      ),
     );
   }
 }
